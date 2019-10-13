061ce933edccef78130db3b9c55941f1a6d03638
Daniele600 <dolivotti@unicef.org>
2019-10-13 11:48:16 +0200

    First commit

---
+++ dotstat_data_explorer.php
@@ -0,0 +1,231 @@
+<?php
+
+/**
+ * Plugin Name: data_explorer
+ * Description: A dotstat data explorer wrapper.
+ * Version: 1.0
+ * Author: Daniele Olivotti
+ */
+
+
+/* ------------------------------------------------------------------------- *
+*   CUSTOM POST TYPE DataExplorer
+/* ------------------------------------------------------------------------- */
+
+add_action('init', 'create_data_explorer');
+
+function create_data_explorer()
+{
+	$labels = array(
+		'name'               => __('Data explorers', 'data_explorer'),
+		'singular_name'      => __('Data explorer', 'data_explorer'),
+		'add_new'            => __('Add new', 'data_explorer'),
+		'add_new_item'       => __('Add New de', 'data_explorer'),
+		'edit_item'          => __('Edit de', 'data_explorer'),
+		'new_item'           => __('New de', 'data_explorer'),
+		'all_items'          => __('All de', 'data_explorer'),
+		'view_item'          => __('View de', 'data_explorer'),
+		'search_items'       => __('Search de', 'data_explorer'),
+		'not_found'          => __('De not found', 'data_explorer'),
+		'not_found_in_trash' => __('De not found in the trash', 'data_explorer'),
+	);
+
+	$args = array(
+		'labels' => $labels,
+		'public' => true,
+		'menu_position' => 22,
+		'supports' => array('title', 'excerpt'),
+		'taxonomies' => array(''),
+		'menu_icon' => 'dashicons-chart-bar',
+		'has_archive' => false
+	);
+
+	register_post_type('data_explorer', $args);
+}
+
+add_action('admin_init', 'my_admin');
+
+function my_admin()
+{
+	add_meta_box(
+		'data_explorer_meta_box',
+		'Data explorer details',
+		'display_de_meta_box',
+		'data_explorer'
+	);
+}
+
+?>
+
+<?php
+function display_de_meta_box($de)
+{
+	$api_url = get_post_meta($de->ID, 'api_url', true);
+	$backtype_radio_value = get_post_meta($de->ID, 'backtype_radio_value', true);
+	?>
+
+	<table>
+		<tr>
+			<td>API URL</td>
+
+			<td>​<textarea name="api_url_name" rows="10" cols="80"><?php echo $api_url; ?></textarea></td>
+		</tr>
+
+		<tr>
+			<td>Backend</td>
+			<td>
+				<label>
+					<input type="radio" name="backtype_radio_value" value="DOTSTAT" <?php checked($backtype_radio_value, 'DOTSTAT'); ?>>
+					<?php esc_attr_e('DOTSTAT'); ?>
+				</label>
+				<br />
+				<label>
+					<input type="radio" name="backtype_radio_value" value="FUSION" <?php checked($backtype_radio_value, 'FUSION'); ?>>
+					<?php esc_attr_e('FUSION'); ?>
+				</label>
+			</td>
+		</tr>
+	</table>
+<?php
+}
+?>
+
+
+<?php
+add_action('save_post', 'add_de_fields', 10, 2);
+
+function add_de_fields($de_id, $de_fields)
+{
+	// Check post type for movie reviews
+	if ($de_fields->post_type == 'data_explorer') {
+		// Store data in post meta table if present in post data
+		if (isset($_POST['api_url_name']) && $_POST['api_url_name'] != '') {
+			update_post_meta($de_id, 'api_url', $_POST['api_url_name']);
+		}
+
+		if (isset($_POST['backtype_radio_value'])) { // Input var okay.
+			update_post_meta($de_id, 'backtype_radio_value', sanitize_text_field(wp_unslash($_POST['backtype_radio_value']))); // Input var okay.
+		} else {
+			update_post_meta($de_id, 'backtype_radio_value', 'DOTSTAT');
+		}
+
+
+		if (isset($_POST['algolia_search_enabled'])) {
+			update_post_meta($de_id, 'algolia_search_enabled', "1");
+		} else {
+			update_post_meta($de_id, 'algolia_search_enabled', "0");
+		}
+
+		update_post_meta($de_id, 'algolia_app_name', $_POST['algolia_app_name']);
+		update_post_meta($de_id, 'algolia_public_key', $_POST['algolia_public_key']);
+		update_post_meta($de_id, 'algolia_index_name', $_POST['algolia_index_name']);
+		update_post_meta($de_id, 'algolia_max_results', $_POST['algolia_max_results']);
+
+		//return $post;
+	}
+}
+?>
+
+<?php
+add_filter('single_template', 'data_explorer_template');
+
+function data_explorer_template($single)
+{
+	global $post;
+
+	$deSinglePath = plugin_dir_path(__FILE__) . 'single-data_explorer.php';
+
+	if ($post->post_type == 'data_explorer') {
+		if (file_exists($deSinglePath)) {
+			return $deSinglePath;
+		}
+	}
+	return $single;
+}
+?>
+
+
+<?php
+function custom_query_vars_filter($vars)
+{
+	$vars[] .= 'ag';
+	$vars[] .= 'df';
+	$vars[] .= 'ver';
+	$vars[] .= 'dq';
+	$vars[] .= 'startPeriod';
+	$vars[] .= 'endPeriod';
+
+	return $vars;
+}
+add_filter('query_vars', 'custom_query_vars_filter');
+?>
+
+
+<?php
+function add_dataexplorer()
+{
+	if (is_single() && get_post_type() == 'data_explorer') {
+
+		$js_url = plugins_url('js/', __FILE__);
+		wp_enqueue_script('de_settings', $js_url . 'de_settings/settings.js', NULL, 1.02, true);
+
+		$static_path = plugin_dir_path(__FILE__) . 'de/static/';
+
+		$css_url = plugins_url('de/static/css/', __FILE__);
+		$js_url = plugins_url('de/static/js/', __FILE__);
+
+		$css_list = glob($static_path . "css/*.css");
+		$js_list = glob($static_path . "js/*.js");
+		//att the styles
+		for ($i = 0; $i < count($css_list); $i++) {
+			//remove the main as it seems to contain body elements already present in the howsting Wordpress page
+			if (strpos(basename($css_list[$i]), "main") === false) {
+				wp_enqueue_style('de_style' . $i, $css_url . basename($css_list[$i]));
+			}
+		}
+
+		wp_enqueue_style('de_style2' . $i, 'C:\xampp\htdocs\wp-content\plugins\wp_dotstat_data_explorer/de/static/css/2.736207e5.chunk.css.map');
+
+		//add the js files in the same order they're added by react in the main page
+		$js_load_order = array();
+		$pos_count = 1;
+
+		//sort them
+		for ($i = 0; $i < count($js_list); $i++) {
+			$baseN = basename($js_list[$i]);
+			if (strpos($baseN, 'runtime~') !== false) {
+				$js_load_order[0] = $baseN;
+			} elseif (strpos($baseN, 'main.') !== false) {
+				$js_load_order[count($js_list) - 1] = $baseN;
+			} else {
+				$js_load_order[$pos_count] = $baseN;
+				$pos_count++;
+			}
+		}
+
+		//add the first one, jquery is a dependency
+		wp_enqueue_script('de_script0', $js_url . $js_load_order[0], array('jquery', 'de_settings',), NULL, true);
+		//add the remaining ones(each one depends on the first one)
+		for ($i = 1; $i < count($js_list); $i++) {
+			wp_enqueue_script('de_script' . $i, $js_url . $js_load_order[$i], array('de_script0'), NULL, true);
+		}
+	}
+}
+
+function add_de()
+{
+	if (is_single() && get_post_type() == 'data_explorer') {
+
+		$css_url = plugins_url('css/', __FILE__);
+		wp_enqueue_style('data_expl_css', $css_url . 'data_explorer.css?v=2');
+		
+		// $js_url = plugins_url('js/', __FILE__);
+		// wp_enqueue_script('related_search', $js_url . 'related_search.js', array('jquery', 'algoliasearchLite', 'algolia_instantsearch'), NULL, true);
+	}
+}
+
+
+//add the scripts and css only if the post type is data_explorer
+add_action('wp_enqueue_scripts', 'add_de');
+add_action('wp_enqueue_scripts', 'add_dataexplorer');
+?>
---
+++ single-data_explorer.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Template Name: data_explorer
+*/
+?>
+<?php get_header(); ?>
+
+
+
+<main id="mainUpdateBrowser" role="main" style="display:none">
+    <?php echo ("global" . $is_IE); ?>
+    <div id="browserError">
+        <div class="innerBrowserError">
+            <h3>Your browser is not supported</h3>
+            <p><strong>Please update your browser and try again</strong></p>
+            <i>ES script is needed</i>
+        </div>
+        <div>
+</main>
+<main id="main" role="main">
+    <script>
+        //Check if ES6 features are available
+        ES6_Error = false;
+
+        function check() {
+            "use strict";
+
+            if (typeof Symbol == "undefined") return false;
+            try {
+                eval("class A {}");
+                eval("var B = (x) => x+1");
+            } catch (e) {
+                return false;
+            }
+
+            return true;
+        }
+        // The engine supports ES6 features you want to use
+        if (!check()) {
+            ES6_Error = true;
+            document.getElementById('main').style.display = "none";
+            document.getElementById('mainUpdateBrowser').style.display = "";
+        }
+    </script>
+
+    <?php
+    //We're in single page, jsut one post at the time, fix this while loop?
+    while (have_posts()) {
+        the_post();
+        $api_url =  get_post_meta(get_the_ID(), 'api_url', true);
+        //$api_url_id = get_post_meta(get_the_ID(), 'agency_id', true);
+        echo ('<script>SETTINGS_override = ' . $api_url . '</script>');
+    }
+
+    $qs_agency = sanitize_text_field(get_query_var('ag'));
+    $qs_dataflow = sanitize_text_field(get_query_var('df'));
+    $qs_version = sanitize_text_field(get_query_var('ver'));
+    $dataquery = sanitize_text_field(get_query_var('dq'));
+
+    $dataquery = $dataquery . "/";
+
+    $startPeriod = sanitize_text_field(get_query_var('startPeriod'));
+    $endPeriod = sanitize_text_field(get_query_var('endPeriod'));
+
+    $backendId =  get_post_meta(get_the_ID(), 'backtype_radio_value', true);
+
+    $script_dataflow = '<script>var DATAFLOW = {datasourceId:"fusion", agencyId:"$agency_id", dataflowId:"$dataflow", version:"$version", dataquery:"$dataquery", period: [$startPeriod, $endPeriod], backendId:"$backendId"}</script>';
+
+    $dataflow_vars = array(
+        '$agency_id' => $qs_agency,
+        '$dataflow' => $qs_dataflow,
+        '$version' =>  $qs_version,
+        '$dataquery' => $dataquery,
+        '$startPeriod' => $startPeriod,
+        '$endPeriod' => $endPeriod,
+        '$backendId' => $backendId
+    );
+
+    $script_dataflow = strtr($script_dataflow, $dataflow_vars);
+
+    echo ($script_dataflow);
+    ?>
+
+    <div id="root">
+    </div>
+</main>
+
+
+
+
+<?php wp_reset_query(); ?>
+<?php get_footer(); ?>
