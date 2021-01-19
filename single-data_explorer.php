d4a94591ab257403e0d6a24c53856e8ef18ab855
Daniele <daoivot@tin.it>
2021-01-19 10:13:57 +0100

    Scripts moved to bundle

--- single-data_explorer.php
+++ single-data_explorer.php
@@ -16,36 +16,14 @@ Template Name: data_explorer
         <div>
 </main>
 <main id="main" class="data-explorer" role="main">
-    <script>
-        //Check if ES6 features are available
-        ES6_Error = false;
 
-        function check() {
-            "use strict";
-
-            if (typeof Symbol == "undefined") return false;
-            try {
-                eval("class A {}");
-                eval("var B = (x) => x+1");
-            } catch (e) {
-                return false;
-            }
-
-            return true;
-        }
-        // The engine supports ES6 features you want to use
-        if (!check()) {
-            ES6_Error = true;
-            document.getElementById('main').style.display = "none";
-            document.getElementById('mainUpdateBrowser').style.display = "";
-        }
-    </script>
 
     <?php
 
     $remote_files_path = "http://localhost/wp-content/plugins/wp_dotstat_data_explorer_cfg";
-    //$remote_files_path="https://data.unicef.org/wp-content/plugins/dataexplorer_maps";
+    //$remote_files_path="https://data.unicef.org/wp-content/plugins/wp_dotstat_data_explorer_cfg";
     //$remote_files_path="https://seotest.buzz/wp-content/plugins/dataexplorer_maps";
+
 
 
 
@@ -53,76 +31,10 @@ Template Name: data_explorer
     //We're in single page, jsut one post at the time, fix this while loop?
     while (have_posts()) {
         the_post();
-        $api_url =  get_post_meta(get_the_ID(), 'api_url', true);
-        //echo ('<script>SETTINGS_override = ' . $api_url . '</script>');
-
-        $indicator_profile_url = esc_attr(get_option('de_indicator_profile_url', ''));
-        $help_url = esc_attr(get_option('de_help_url', ''));
-        $de_hide_total_labels = 0;
-        if (get_post_meta(get_the_ID(), 'de_hide_total_labels', true) == "on") {
-            $de_hide_total_labels = 1;
-        }
-
-        $unicef_settings = '{"indicatorProfileUrl": "' . $indicator_profile_url . '", "helpUrl": "' . $help_url . '", "hideTotalLabel":' . $de_hide_total_labels . '}';
-        //$unicef_settings = '{"indicatorProfileUrl": "' . $indicator_profile_url . '", "helpUrl": "' . $help_url . '" }';
-        //echo ('<script>unicef_settings = ' . $unicef_settings . '</script>');
-
-        $map_settings = '{
-            "UNICEF:GLOBAL_DATAFLOW":{"ref_area_dim_id":"REF_AREA", "geojson_url": "' . $remote_files_path . '/maps/CNTR_RG_20M_2020_4326.json"},
-            "UNICEF:CME":{"ref_area_dim_id":"REF_AREA", "geojson_url": "' . $remote_files_path . '/maps/test.json"}
-        }';
-
-        //echo ('<script>map_settings = ' . $map_settings . '</script>');
-
-        $hierarchy = get_post_meta(get_the_ID(), 'de_hierarchy_cfg', true);
-        if ($hierarchy != null && trim($hierarchy) != "") {
-            echo ('<script>HIERARCHY_override=' . $hierarchy . '</script>');
-        } else {
-            echo ('<script>HIERARCHY_override={}</script>');
-        }
-
-        $de_forced_dims = get_post_meta(get_the_ID(), 'de_forced_dims', true);
     }
 
-    $qs_agency = sanitize_text_field(get_query_var('ag'));
-    $qs_dataflow = sanitize_text_field(get_query_var('df'));
-    $qs_version = sanitize_text_field(get_query_var('ver'));
-    $dataquery = sanitize_text_field(get_query_var('dq'));
 
-    $dataquery = $dataquery . "/";
-
-    $startPeriod = sanitize_text_field(get_query_var('startPeriod'));
-    $endPeriod = sanitize_text_field(get_query_var('endPeriod'));
-    if ($endPeriod == "") {
-        $endPeriod = date("Y");
-    }
-
-    $backendId =  get_post_meta(get_the_ID(), 'backtype_radio_value', true);
-
-    $script_dataflow = '<script>var DATAFLOW = {datasourceId:"fusion", agencyId:"$agency_id", dataflowId:"$dataflow", version:"$version", dataquery:"$dataquery", period: [$startPeriod, $endPeriod], backendId:"$backendId"';
-
-    if ($de_forced_dims != "") {
-        $script_dataflow = $script_dataflow . ',forcedDims:$forcedDims}</script>';
-    } else {
-        $script_dataflow = $script_dataflow . '}</script>';
-    }
-
-    $dataflow_vars = array(
-        '$agency_id' => $qs_agency,
-        '$dataflow' => $qs_dataflow,
-        '$version' =>  $qs_version,
-        '$dataquery' => str_replace(" ", "+", $dataquery),
-        '$startPeriod' => $startPeriod,
-        '$endPeriod' => $endPeriod,
-        '$backendId' => $backendId,
-        '$forcedDims' => $de_forced_dims,
-    );
-
-    $script_dataflow = strtr($script_dataflow, $dataflow_vars);
-
-    //echo ($script_dataflow);
-
-    echo ("<script>var remote_files_path=" . $remote_files_path . " </script>");
+    echo ("<script>var remote_files_path=\"" . $remote_files_path . "\" </script>");
 
     ?>
 
@@ -153,60 +65,21 @@ Template Name: data_explorer
 
     <div id="root">
     </div>
-
-    <?php if ($help_url != null && $help_url != "") { ?>
-        <div id="div_de_help" class="pull-right">
-            <div class="closebtn" onclick="document.getElementById('div_de_help').style.display='none';"><i class="material-icons md-18">clear</i></div>
-            <a href="<?php echo ($help_url) ?>"><i class="material-icons">help</i></a><a href="<?php echo ($help_url) ?>"><span class="help_text">Need help using this tool?</span><span class="help_text_small">Help</span>
-            </a>
-        </div>
-    <?php } ?>
 </main>
 
-<?php $res_v = "1.09" ?>
-
-<link rel="stylesheet" href="<?php echo ($remote_files_path); ?>/css/data_explorer.css?v=<?php echo ($res_v); ?>" />
-<link rel="stylesheet" href="<?php echo ($remote_files_path); ?>/de/static/css/main.chunk.css?v=<?php echo ($res_v); ?>" />
-<link rel="stylesheet" href="<?php echo ($remote_files_path); ?>/de/static/css/2.chunk.css?v=<?php echo ($res_v); ?>" />
-
-
-
-<script src="<?php echo ($remote_files_path); ?>/js/de_settings/settings.js?v=<?php echo ($res_v); ?>"></script>
-<script src="<?php echo ($remote_files_path); ?>/js/url_changer.js?v=<?php echo ($res_v); ?>"></script>
 
 
 
 <script>
     var remote_files_path = "<?php echo ($remote_files_path); ?>"
+    var json_config = "PCO_subnat";
+    var version="1.09";
 </script>
-<script>
-    var myUrl = document.location.host + document.location.pathname;
-    var myUrlMD5 = calculateMD5(myUrl);
-    console.log("MY URL")
-    console.log(myUrl);
-    console.log(myUrlMD5);
+
+<script src="<?php echo ($remote_files_path); ?>/js/bundle.js?v="+version></script>
 
 
-    loadJson(remote_files_path + "/configs/" + myUrlMD5 + ".json", function(data) {
-        /*
-        console.log(data)
-        var node=document.createElement('script');
-        node.innerHTML="var react_settings="+data;
-        document.body.appendChild(node);
-        console.log(node);
-        var node=document.createElement('script');
-        var val_dataflow=JSON.parse(data)["DATAFLOW"];
-        
 
-        console.log("var DATAFLOW="+JSON.stringify(val_dataflow));
-        console.log("PPPP2")
-        node.innerHTML="var DATAFLOW="+JSON.stringify(val_dataflow);
-        document.body.appendChild(node);
-        console.log(node);
-        */
-        addReactScripts(JSON.parse(data), remote_files_path, "1.00");
-    });
-</script>
 <?php
 wp_reset_query();
 get_footer();
