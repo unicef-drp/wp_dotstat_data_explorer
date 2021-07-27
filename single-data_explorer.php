4ee5b021332b35b794da5ef85ece03a971278d7d
Daniele <daoivot@tin.it>
2021-07-27 10:39:42 +0200

    Remote configuration files location override removed

--- single-data_explorer.php
+++ single-data_explorer.php
@@ -22,7 +22,9 @@ Template Name: data_explorer
     $remote_files_path = esc_attr(get_option('de_remote_files_url', ''));
     $de_config_id =  get_post_meta(get_the_ID(), 'de_config_id', true);
 
-    $remote_files_path="https://data.unicef.org/wp-content/plugins/wp_dotstat_data_explorer_cfg";
+    echo($remote_files_path);
+
+    //$remote_files_path="https://data.unicef.org/wp-content/plugins/wp_dotstat_data_explorer_cfg";
     //$remote_files_path = "http://localhost/wp-content/plugins/wp_dotstat_data_explorer_cfg";
     //$remote_files_path="http://seotest.buzz/wp-content/plugins/dataexplorer_maps";
 
